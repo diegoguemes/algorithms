78010f7c74f3149edd02020822713e442249eccf
Diego Güemes <diegogp1988@gmail.com>
2013-12-10 22:22:04 -0800

    Change parameter name

--- peak_finding/peak_finder.rb
+++ peak_finding/peak_finder.rb
@@ -1,14 +1,14 @@
-def find_1d_peak(list)
+def find_1d_peak(a)
   left = 0
-  right = list.length - 1
+  right = a.length - 1
   while left <= right
     middle = (left + right) / 2
-    if middle > 0 and list[middle - 1] > list[middle]
+    if middle > 0 and a[middle - 1] > a[middle]
       right = middle - 1
-    elsif middle < list.length - 1 and list[middle + 1] > list[middle]
+    elsif middle < a.length - 1 and a[middle + 1] > a[middle]
       left = middle + 1
     else
-      return list[middle]
+      return a[middle]
     end
   end
 end
